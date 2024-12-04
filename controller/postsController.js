e21adde498765625d1c707170a138ecdf3718ce4
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-12-04 14:42:14 +0100

    feat: milestone #2 complete crud logic implementation

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,5 +1,7 @@
+// Data
 const postsData = require("../data/posts");
 
+// Index
 const index = (req, res) => {
     res.json({
         post: postsData,
@@ -7,29 +9,55 @@ const index = (req, res) => {
     });
 };
 
+// Show
 const show = (req, res) => {
     const postId = parseInt(req.params.id);
     const post = postsData.find((curPost) => curPost.id === postId);
-    res.json(post);
+
+    if (post === undefined) {
+        res.statusCode = 404;
+        res.json({
+            error: true,
+            message: "Post non trovato!"
+        });
+    } else {
+        res.json(post);
+    }
 };
 
+// Create
 const create = (req, res) => {
     res.json("Creo un nuovo elemento!");
 };
 
+// Update
 const update = (req, res) => {
     const postId = req.params.id;
     res.json("Modifico i dati dell'elemento " + postId);
 };
 
+// Modify
 const modify = (req, res) => {
     const postId = req.params.id;
     res.json("Modifico alcuni dati dell'elemento " + postId);
 };
 
+// Destroy
 const destroy = (req, res) => {
-    const postId = req.params.id;
-    res.json("Cancello l'elemento " + postId);
+    const postId = parseInt(req.params.id);
+    const postIndex = postsData.findIndex((curPost) => curPost.id === postId);
+
+    if(postIndex === -1) {
+        res.statusCode = 404;
+        res.json({
+            error: true,
+            message: "Post non trovato!",
+        });
+    } else {
+        postsData.splice(postIndex, 1);
+        console.log(postsData);
+        res.sendStatus(204);
+    }
 };
 
 // Export
--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,4 @@
+// Data
 const express = require("express");
 const router = express.Router();
 const postsData = require("../data/posts");
